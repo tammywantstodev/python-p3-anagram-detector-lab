beed0614ffa61a03b6734fd661cd77155320776e
Tamara Mwanzali Kaka <billnyetherussianspy679@gmail.com>
2025-02-14 14:56:29 +0300

    saving changes

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,17 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word=word
+
+    def match(self, anagrams):
+        matches=[]
+        for anagram in anagrams:
+           if sorted(anagram) == sorted(self.word):
+               matches.append(anagram)
+        return matches 
+
+    
+
+listen = Anagram("listen")
+listen.match(['enlists', 'google', 'inlets', 'banana'])
+# => ['inlets']
